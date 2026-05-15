5d20337a36e550f52ae13ce9884223abb0859ce7
Atul5486 <atulkoushal5486@gmail.com>
2026-05-15 18:41:13 +0530

    Billing and payment

---
+++ server/controllers/webhook.controller.js
@@ -0,0 +1,29 @@
+import stripeInstance from "../config/stripe.js";
+import userModel from '../models/user.model.js'
+
+export const stripeWebHook=async(req,res)=>{
+    const signature=req.headers["stripe-signature"]
+    let event;
+    try {
+        event=stripeInstance.webhooks.constructEvent(
+            req.body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET
+        )
+    } catch (error) {
+        res.status(500).json({message:"Error in webhook"+error.message});
+    }   
+
+    if(event.type=="checkout.session.completed"){
+        const session=event.data.object
+        const userId=session.metadata.userId
+        const plan=session.metadata.plan
+        const credits=Number(session.metadata.credits)
+
+        await userModel.findByIdAndUpdate(userId,{
+            $inc:{credits},
+            plan
+        })
+    }
+    return res.json({received:true})
+}

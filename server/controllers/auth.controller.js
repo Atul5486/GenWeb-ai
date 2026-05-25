92e85569d52b39c28515e5ab9e0fd5f5e3760df3
Atul5486 <atulkoushal5486@gmail.com>
2026-05-25 22:51:23 +0530

    change in controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -27,8 +27,8 @@ export const googleAuth=async(req,res)=>{
     const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'})
     res.cookie("token",token,{
         httpOnly:true,
-        secure:false,
-        sameSite:"strict",
+        secure:true,
+        sameSite:"none",
         maxAge:7*24*60*60*1000
     });
     res.status(201).json(user);
